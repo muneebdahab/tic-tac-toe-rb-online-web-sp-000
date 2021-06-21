81502c6e63468f33bfb9544b36200ccf8d5c557e
Muneeb Dahab <muneeb.dahab@outlook.com>
2021-06-21 21:25:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,8 +32,6 @@ def valid_move?(board, index)
    end
 end
 
-
-
 def turn_count(board)
    counter = 0
    board.each do |spaces|
@@ -48,14 +46,25 @@ def current_player(board)
 end
 
 
-   if won?(board)
-      winner(board) == "X" || winner(board) == "O" 
-      puts "Congratulations #{winner(board)}!" 
-   elsif draw?(board)
-      puts "Cats Game!" 
-   end 
+def play(board)
+   counter = 0
+   until counter == 9
+     turn(board)
+     counter += 1
+   end
 end
 
+def play(board)
+   until over?(board) 
+      turn(board)
+   end 
+   if won?(board)
+      won?(board) == "X" || won?(board) == "O" 
+      puts "Congratulations #{winner(board)}!" 
+   elsif draw?(board)
+      puts "Cat's Game!" 
+   end 
+end
 
 def turn(board)
    puts "Please enter 1-9:"
@@ -68,24 +77,29 @@ def turn(board)
    display_board(board)
 end
 
+
+WIN_COMBINATIONS = [ 
+[0,1,2],  
+[3,4,5], 
+[6,7,8], 
+[0,3,6], 
+[1,4,7], 
+[2,5,8], 
+[0,4,8], 
+[6,4,2] 
+]
+
 def won?(board)
  WIN_COMBINATIONS.each do |win_combination|
    win_index_1 = win_combination[0]
    win_index_2 = win_combination[1]
    win_index_3 = win_combination[2]
-   position_1 = board[win_index_1] # value of board at win_index_1
-   position_2 = board[win_index_2] # value of board at win_index_2
-   position_3 = board[win_index_3] # value of board at win_index_3
+   position_1 = board[win_index_1] 
+   position_2 = board[win_index_2]
+   position_3 = board[win_index_3] 
    position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
  end
 end
-
-position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1) 
-
-
-#position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1) 
-
-
 
 def full?(board)
    board.all? {|i| i == "X" || i == "O"}
